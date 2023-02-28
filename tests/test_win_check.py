02bc33ad563cb12a75b3256e44683f3fe3280e67
Olga Khomenko <homenko.olga@gmail.com>
2023-02-27 19:13:27 -0500

    More win check tests

--- tests/test_win_check.py
+++ tests/test_win_check.py
@@ -2,45 +2,56 @@ import game
 from pytest import mark, param
 
 
-def columns_board(position, marker):
-    board = [' '] * 9
-    for i in range(position, 9, 3):
-        board[i] = marker
-    print(board)
-    return board
-
-
 class TestWinCheck:
 
     @mark.parametrize(
-        "marker",
+        "marker, position",
         [
-            param('X', id="X marker"),
-            param('Y', id="Y marker")
+            param('X', 0, id="X marker at 0 position"),
+            param('Y', 0, id="Y marker at 0 position"),
+            param('X', 1, id="X marker at 1 position"),
+            param('Y', 1, id="Y marker at 1 position"),
+            param('X', 2, id="X marker at 2 position"),
+            param('Y', 2, id="Y marker at 2 position")
         ])
-    def test_left_column(self, marker):
-        board = columns_board(0, marker)
+    def test_columns(self, marker, position):
+        board = [' '] * 9
+        for i in range(position, 9, 3):
+            board[i] = marker
+        print(board)
 
         assert game.win_check(board, marker)
 
     @mark.parametrize(
-        "marker",
+        "marker, position",
         [
-            param('X', id="X marker"),
-            param('Y', id="Y marker")
+            param('X', 0, id="X marker at 0 position"),
+            param('Y', 0, id="Y marker at 0 position"),
+            param('X', 3, id="X marker at 1 position"),
+            param('Y', 3, id="Y marker at 1 position"),
+            param('X', 6, id="X marker at 2 position"),
+            param('Y', 6, id="Y marker at 2 position")
         ])
-    def test_center_column(self, marker):
-        board = columns_board(1, marker)
+    def test_rows(self, marker, position):
+        board = [' '] * 9
+        for i in range(position, position + 3):
+            board[i] = marker
+        print(board)
 
         assert game.win_check(board, marker)
 
     @mark.parametrize(
-        "marker",
+        "marker, positions",
         [
-            param('X', id="X marker"),
-            param('Y', id="Y marker")
+            param("X", [2, 4, 6], id="X diagonal right to left"),
+            param("X", [0, 4, 8], id="X diagonal left to right"),
+            param("Y", [2, 4, 6], id="Y diagonal right to left"),
+            param("Y", [0, 4, 8], id="Y diagonal left to right"),
         ])
-    def test_right_column(self, marker):
-        board = columns_board(2, marker)
+    def test_diagonals(self, marker, positions):
+        board = [' '] * 9
+        for position in positions:
+            board[position] = marker
+        print(board)
 
         assert game.win_check(board, marker)
